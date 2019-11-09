fc0d4204bc584b9df0966e63f326c74b6e96beef
Deepak Kumar Sood <deepaksood619@gmail.com>
2019-11-09 22:20:12 +0530

    food menu and order models added

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from sales.models import FoodAvailabilityTimings, Food, Restaurant, Order, OrderedItems
+
+
+class FoodAvailabilityTimingsAdmin(admin.ModelAdmin):
+    list_display = ('shift_name', 'start_time', 'end_time')
+
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('item_name', 'price')
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('food', 'quantity')
+
+
+class OrderedItemsAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'restaurant', 'bill_amount', 'timestamp')
+
+
+admin.site.register(FoodAvailabilityTimings, FoodAvailabilityTimingsAdmin)
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderedItems, OrderedItemsAdmin)
--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,48 @@
+from datetime import time, datetime
+
 from django.db import models
 
+
 # Create your models here.
+class FoodAvailabilityTimings(models.Model):
+    shift_name = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    start_time = models.TimeField(default=time(hour=0, minute=0))
+    end_time = models.TimeField(default=time(hour=23, minute=59))
+
+    def __str__(self):
+        return f'{self.shift_name}'
+
+
+class Food(models.Model):
+    item_name = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    available_time = models.ManyToManyField(to=FoodAvailabilityTimings, blank=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.item_name}'
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Order(models.Model):
+    food = models.ForeignKey(Food, blank=False, null=False, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.food}'
+
+
+class OrderedItems(models.Model):
+    order_id = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    restaurant = models.ForeignKey(to=Restaurant, blank=False, null=False, on_delete=models.CASCADE)
+    items = models.ManyToManyField(to=Order, blank=False)
+    bill_amount = models.PositiveIntegerField(blank=False, null=False)
+    timestamp = models.DateTimeField(default=datetime.now, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.order_id}'
